22d0c6f52b8c06bd5adae1c51ef37253d9a1187c
pebalmeida <93488924+pebalmeida@users.noreply.github.com>
2021-11-15 12:09:37 -0300

    Add files via upload

---
+++ texto.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+class texto{
+    public static void main(String args[]){
+        String TEXTO = "Eu gosto de jogar FIFA 2022";
+        int TAMANHO = TEXTO.length();
+        System.out.println(TAMANHO);
+    }
+}
