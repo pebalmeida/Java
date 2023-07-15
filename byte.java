5618a85f6c43d185d595c2c8940990034cfc0238
pebalmeida <periclesbbbello05@gmail.com>
2023-07-15 18:29:17 -0300

    Java

---
+++ byte.java
@@ -0,0 +1,10 @@
+class Byte{
+	public static void main(String args[]){
+	byte CURTO = 20;
+	String = STR;
+	STR = Byte.toString(CURTO);
+	System.out.println();
+	System.out.print("Valor = " + STR);
+	System.out.println();
+	}
+}
